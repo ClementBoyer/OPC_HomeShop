2d7a64f00335418ee0452a481522dfbf7da67c02
80010-37-22 <boyer.clement95@gmail.com>
2018-10-24 16:04:05 +0200

    Update Customer

--- src/com/clement/homeshop/Customer.java
+++ src/com/clement/homeshop/Customer.java
@@ -5,6 +5,12 @@ public class Customer
     private String fullname;
     private String address;
 
+    public  Customer (String fullname, String address)
+    {
+        this.fullname=fullname;
+        this.address=address;
+    }
+
     public String getFullname()
     {
         return fullname;
